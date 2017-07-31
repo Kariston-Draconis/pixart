7617b808a9498a455a1f1256f7ba6ac26e8fa925
Alex May <thrawnrulz@hotmail.com>
2017-07-31 16:19:46 +1000

    squares added to page

--- pixart.js
+++ pixart.js
@@ -1,8 +1,14 @@
-var button = document.getElementById('set-color')
-var brush = document.getElementsByClassName('brush')[0]
+var button = document.getElementById('set-color');
+var brush = document.getElementsByClassName('brush')[0];
 
 button.addEventListener('click', function(event) {
   event.preventDefault();
   var color = document.getElementById('color-field').value;
   brush.style.backgroundColor = color;
-})
+});
+
+for (var i = 0; i < 20; i++) {
+  var square = document.createElement('div');
+  square.className = "square"
+  document.body.appendChild(square);
+};
